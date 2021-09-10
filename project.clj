3256750827729debcc45bc476c5aacdffb113ebf
jef <324976+xbrln@users.noreply.github.com>
2021-09-10 13:27:35 +0200

    Update project file

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject weather-api-ring "0.1.0"
+(defproject  weather-api-ring "0.1.0"
   :description "API to fetch temperature for a city"
   :url "https://github.com/xbrln/weather-api-ring"
+  :min-lein-version "2.0.0"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
@@ -13,8 +14,9 @@
                  [org.clojure/java.jdbc "0.6.1"]
                  [org.postgresql/postgresql "42.2.23"]
                  [environ "1.2.0"]]
-  :plugins [[lein-environ "1.2.0"]]
   :main ^:skip-aot weather-api-ring.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :uberjar-name "weather-api-ring-standalone.jar"
+  :plugins [[lein-environ "1.2.0"]]
+  :ring {:handler weather-api-ring.core/app
+         :init weather-api-ring.db/migrate}
+  :profiles {:uberjar {:aot :all}})
