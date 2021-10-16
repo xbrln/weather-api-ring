e19645c201411ae6ad5bc2b73377a55ccdc1e51e
jef <324976+xbrln@users.noreply.github.com>
2021-10-16 17:27:21 +0200

    Add query params with wrap-params

--- src/weather_api_ring/core.clj
+++ src/weather_api_ring/core.clj
@@ -1,24 +1,27 @@
 (ns weather-api-ring.core
   (:require
-   [compojure.core :as comp]
+   [compojure.core :as compojure]
    [compojure.route :as route]
    [environ.core :refer [env]]
    [ring.adapter.jetty :as jetty]
    [ring.middleware.json :refer [wrap-json-response]]
+   [ring.middleware.params :refer [wrap-params]]
    [ring.util.response :refer [response]]
    [weather-api-ring.controllers.weather :as w]
    [weather-api-ring.models.api-calls :as db])
   (:gen-class))
 
-(comp/defroutes
+(compojure/defroutes
  routes
- (comp/GET "/" [] (response "weather api wrapper"))
- (comp/GET "/weather/:city" [city] (response (w/weather city)))
- (comp/GET "/weather" [] (response (w/all-weather)))
+ (compojure/GET "/" [] (response "weather api wrapper"))
+ (compojure/GET "/weather/:city" [city] (response (w/weather city)))
+ (compojure/GET "/weather" [] (response (w/all-weather)))
+ (compojure/GET "/qp" {qp :query-params} (response qp))
  (route/not-found (response "Not found")))
 
 (def app (-> #'routes
-             wrap-json-response))
+             wrap-json-response
+             wrap-params))
 
 (defonce server (atom nil))
 
